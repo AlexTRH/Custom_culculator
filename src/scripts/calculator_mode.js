41f93a1323c130f026a36f892125988bfeeb9f27
AlexTRH <19tam99@mail.ru>
2023-07-03 17:07:36 +0300

    Added app demo

--- src/scripts/calculator_mode.js
+++ src/scripts/calculator_mode.js
@@ -14,36 +14,3 @@ export function switchMode() {
 
 switchCalculatorMode.addEventListener('click', switchMode);
 export { switchCalculatorMode };
-
-// const memoryValue = document.getElementById('memoryValue');
-//
-// export default class Calculator_mode {
-// 	constructor() {
-// 		this.value = 0;
-// 		this.history = [];
-// 		this.memory = 0;
-// 	}
-//
-// 	resetMemory() {
-// 		this.memory = 0;
-// 		memoryValue.innerHTML = this.memory;
-// 	}
-//
-// 	changeMemory(value) {
-// 		this.memory += value;
-// 		memoryValue.innerHTML = this.memory;
-// 	}
-//
-// 	resetValue(value) {
-// 		this.value = value;
-// 	}
-//
-// 	executeCommand(command) {
-// 		this.history.push(this.value);
-// 		this.value = command.execute(this.value);
-// 	}
-//
-// 	undo() {
-// 		this.resetValue(this.history.pop());
-// 	}
-// }
